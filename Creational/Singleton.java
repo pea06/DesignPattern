9250764abf7f5587a9bd6f48fbb5d77038e64738
pea06 <101560095+pea06@users.noreply.github.com>
2022-10-14 14:08:24 +0900

    싱글톤 패턴

---
+++ Creational/Singleton.java
@@ -0,0 +1,14 @@
+package Creational;
+
+public class Singleton {
+    private static Singleton instance = new Singleton();
+
+    private Singleton() {
+        //생성자를 외부에서 호출 하지 못하게 private 로 지정 한다
+    }
+
+    public static Singleton getInstance(){
+        return instance;
+    }
+
+}
